cab4f2887c8ca66f3a8e0c85545ce6b3e95afcd7
AHN <gd20624@gmail.com>
2025-01-08 17:37:39 +0900

    My first Cloud Server front with chakraUI

--- src/apis/createfolder/CreateFolder.ts
+++ src/apis/createfolder/CreateFolder.ts
@@ -1,15 +1,14 @@
+import axios from "axios";
+
+const host = `http://localhost:8080`
 
 export const createFolder = async (
     folderName: string
 ): Promise<string> => {
     try{
-        axios.post("http://localhost:3000/create-folder", { folderName })
-            .then((response) => {
-                alert(response.data.message);
-            })
-            .catch((error) => {
-                console.error(error);
-            })
+        const response = await axios.post(`${host}/create-folder`, { folderName });
+            alert(response.data.message);
+            return response.data.message;
     }catch(error){
         if(error){
             console.log(error);
